f9a5a6c255e6851c9e608e9118b865c2ce1aa499
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2020-03-03 11:51:13 +0100

    Update deps for security vulnerabilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     extends: ["plugin:vue/recommended", "plugin:prettier/recommended", "@vue/prettier"],
     rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
         "vue/no-v-html": ["off"]
     },
