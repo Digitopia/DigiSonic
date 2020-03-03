91eaab10887dec3f6433c12fc1b8bc26512cdc06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-03 11:03:16 +0000

    Deploy Digitopia/DigiSonic to github.com/Digitopia/DigiSonic.git:gh-pages

---
+++ precache-manifest.c93bb2231db3823a1ea0d4c3dd09935b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79912b30da4f8bfe6b9",
+    "url": "/DigiSonic/css/app.66722c7b.css"
+  },
+  {
+    "revision": "6a99b57ad0fb1425c5b4",
+    "url": "/DigiSonic/css/chunk-vendors.3ef31df9.css"
+  },
+  {
+    "revision": "5800de278df31888811a53d6f2078d11",
+    "url": "/DigiSonic/index.html"
+  },
+  {
+    "revision": "a79912b30da4f8bfe6b9",
+    "url": "/DigiSonic/js/app.0886c7c9.js"
+  },
+  {
+    "revision": "6a99b57ad0fb1425c5b4",
+    "url": "/DigiSonic/js/chunk-vendors.c816bad0.js"
+  },
+  {
+    "revision": "60840c3b964c4ae55bd8d7782e0926eb",
+    "url": "/DigiSonic/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DigiSonic/robots.txt"
+  },
+  {
+    "revision": "7bca789d6af67e29e0672a842adc3f17",
+    "url": "/DigiSonic/sounds/piano/A3.mp3"
+  },
+  {
+    "revision": "524d7019dbfdba454c8c613aeaec4d40",
+    "url": "/DigiSonic/sounds/piano/A4.mp3"
+  },
+  {
+    "revision": "5ddb4301bd96f002b6a10e6d32806838",
+    "url": "/DigiSonic/sounds/piano/B3.mp3"
+  },
+  {
+    "revision": "5cee4634613551e1cdc687ad3741373e",
+    "url": "/DigiSonic/sounds/piano/Bb3.mp3"
+  },
+  {
+    "revision": "55b304bd79c2a5d5d282d0e8e038c94c",
+    "url": "/DigiSonic/sounds/piano/C3.mp3"
+  },
+  {
+    "revision": "87a47798df6a0bd34b0f7561876a145b",
+    "url": "/DigiSonic/sounds/piano/C4.mp3"
+  },
+  {
+    "revision": "f3acff97732e40892f6fe57660cf7293",
+    "url": "/DigiSonic/sounds/piano/D3.mp3"
+  },
+  {
+    "revision": "90dc611da77921d220f2bf11cd2ea3d6",
+    "url": "/DigiSonic/sounds/piano/D4.mp3"
+  },
+  {
+    "revision": "095cd6d55e30604e223d3a938e0799d1",
+    "url": "/DigiSonic/sounds/piano/Db3.mp3"
+  },
+  {
+    "revision": "b6a2f8919c3572e943a147e788b37cbb",
+    "url": "/DigiSonic/sounds/piano/Db4.mp3"
+  },
+  {
+    "revision": "c334fd2dcabd4165c2d33a902f1ff4d4",
+    "url": "/DigiSonic/sounds/piano/E3.mp3"
+  },
+  {
+    "revision": "360a21079a9316b7088e3ce4b3ea00e1",
+    "url": "/DigiSonic/sounds/piano/E4.mp3"
+  },
+  {
+    "revision": "d54bdf078de24f6fe05bd365d0940986",
+    "url": "/DigiSonic/sounds/piano/F3.mp3"
+  },
+  {
+    "revision": "68dbfd56a5554a69f8536a2e29839ba3",
+    "url": "/DigiSonic/sounds/piano/F4.mp3"
+  },
+  {
+    "revision": "f2088ba48b097eca6981d9210adf74f4",
+    "url": "/DigiSonic/sounds/piano/G3.mp3"
+  },
+  {
+    "revision": "2105f1bcaf86cc89b4271bfab4a87c43",
+    "url": "/DigiSonic/sounds/piano/G4.mp3"
+  },
+  {
+    "revision": "57a9f3b09ef8f3f25da853ca6d55457a",
+    "url": "/DigiSonic/sounds/piano/Gb3.mp3"
+  },
+  {
+    "revision": "3a79b48990a46ec0bc8d40324b9e9c49",
+    "url": "/DigiSonic/sounds/piano/Gb4.mp3"
+  },
+  {
+    "revision": "54422c1bc4e4fc03f409763f9b50df49",
+    "url": "/DigiSonic/svg/repeat.svg"
+  }
+]);
